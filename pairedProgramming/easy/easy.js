f0927c8427681a787653c2082d4cc14bedd5039b
Ultrafeeder <jakarimstratton@gmail.com>
2022-01-25 17:49:10 -0500

    made test branch

--- pairedProgramming/easy/easy.js
+++ pairedProgramming/easy/easy.js
@@ -1,22 +1,22 @@
-// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-// function oddOrEven(num) {
-//     let odd = [];
-//     let even = [];
+function oddOrEven(num) {
+    let odd = [];
+    let even = [];
 
-//     for (let i = 0; i < num.length; i++) {
-//         if (num[i] % 2 === 0) {
-//             even.push(num[i])
-//         } else {
-//             odd.push(num[i])
-//         }
-//     }
-//     return odd && even
-// }
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] % 2 === 0) {
+            even.push(num[i])
+        } else {
+            odd.push(num[i])
+        }
+    }
+    return odd && even
+}
 
 
 
-// console.log(oddOrEven(array))
+console.log(oddOrEven(array))
 
 
 let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
