99f9d51c295ea0b577cb5640d9f5379f7b94c0c9
Ultrafeeder <jakarimstratton@gmail.com>
2022-01-25 16:09:20 -0500

    working

--- pairedProgramming/easy/easy.js
+++ pairedProgramming/easy/easy.js
@@ -1,47 +1,48 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-function oddOrEven(num) {
-    let odd = [];
-    let even = [];
+// function oddOrEven(num) {
+//     let odd = [];
+//     let even = [];
 
-    for (let i = 0; i < num.length; i++) {
-        if (num[i] % 2 === 0) {
-            even.push(num[i])
-        } else {
-            odd.push(num[i])
-        }
-    }
-    return odd && even
-}
+//     for (let i = 0; i < num.length; i++) {
+//         if (num[i] % 2 === 0) {
+//             even.push(num[i])
+//         } else {
+//             odd.push(num[i])
+//         }
+//     }
+//     return odd && even
+// }
 
 
 
-console.log(oddOrEven(array))
+// console.log(oddOrEven(array))
 
 
 let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 function primeOrNot(num) {
-     
+    let prime = [];
+    let not = [];
     if (num <= 1) {
-        return false;
+        not.push(num);
     } else {
         for (let i = 2; i < num; i++) {
             if (num % i == 0) {
-                return false;
-            }
+                not.push(num);
+            }else {
+            prime.push(num)
+             }
         }
-        return true;
+        
     }
 }
     
 
-array2.map(function (x) {
-    let prime = [];
+array2.forEach(function (x) {
     const isPrime = primeOrNot(x);
     if (isPrime) {
-        prime.push(prime);
-        console.log()
+        console.log(primeOrNot(array2))
     }
 });
 
