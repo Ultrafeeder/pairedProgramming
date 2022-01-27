472639090fe11e35633b38d53c39c13087f0225c
Ultrafeeder <jakarimstratton@gmail.com>
2022-01-26 20:20:21 -0500

    finished hard

---
+++ pairedProgramming/hard/hard.js
@@ -0,0 +1,17 @@
+function matchingBrackets(string) {
+    let data = string.split('')
+    let first = data[0];
+    let last = data[data.length - 1]
+    if (first === "[" && last === "]") {
+        return true;
+    } else if (first === "{" && last === "}") {
+        return true;
+    } else if (first === "(" && last === ")") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(matchingBrackets("[whut]"))
